49b903df907752522fea3cc1cf479db40711506d
dtemilade <dtemilade@gmail.com>
2023-04-26 20:43:17 -0700

    precision file updated

--- get_precision.c
+++ get_precision.c
@@ -9,39 +9,39 @@
  */
 int get_precision(const char *format, int *i, va_list list)
 {
-/*introducing parameter to be used*/
-int precision, x;
+	/*introducing parameter to be used*/
+	int precision, x;
 
-/*initializing the parameter*/
-precision = -1;
-x = *i + 1;
+	/*initializing the parameter*/
+	precision = -1;
+	x = *i + 1;
 
-/*conditional statements for precision*/
-if (format[x] != '.')
-return (precision);
+	/*conditional statements for precision*/
+	if (format[x] != '.')
+		return (precision);
 
-precision = 0;
+	precision = 0;
 
-/*check if its digit and then adjust to possible value*/
-for (x += 1; format[x] != '\0'; x++)
-{
-if (is_digit(format[x]))
-{
-precision *= 10;
-precision = precision + format[x] - '0';
-}
-/*maintain the value if its character*/
-else if (format[x] == '*')
-{
-x++;
-precision = va_arg(list, int);
-break;
-}
-else
-break;
-}
-/*finally return precision if condition not met*/
-*i = x - 1;
+	/*check if its digit and then adjust to possible value*/
+	for (x += 1; format[x] != '\0'; x++)
+	{
+		if (is_digit(format[x]))
+		{
+			precision *= 10;
+			precision = precision + format[x] - '0';
+		}
+		/*maintain the value if its character*/
+		else if (format[x] == '*')
+		{
+			x++;
+			precision = va_arg(list, int);
+			break;
+		}
+		else
+			break;
+	}
+	/*finally return precision if condition not met*/
+	*i = x - 1;
 
-return (precision);
+	return (precision);
 }
